65f4b79ef82f1ce876e8fd43e78bcf89e6aabc93
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-01-28 10:11:23 -0300

    req 1 - cria class Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,22 @@
+abstract class Race {
+  constructor(private _name: string, private _dexterity: number) { 
+    this._name = _name;
+    this._dexterity = _dexterity;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
+
+export default Race;
