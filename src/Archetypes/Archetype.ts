4392e1fb03e56f03c353e162ca8efa79888a2229
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-01-28 11:14:35 -0300

    req 4 - created class Archetype

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,31 @@
+import { EnergyType } from '../Energy';
+
+abstract class Archetype {
+  constructor(
+    private _name: string,
+    private _special: number = 0,
+    private _cost: number = 0,
+  ) {
+    this._name = _name;
+  }
+  
+  get name(): string {
+    return this._name;
+  }
+
+  get special(): number {
+    return this._special;
+  }
+
+  get cost(): number {
+    return this._cost;
+  }
+
+  static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType(): EnergyType;
+}
+
+export default Archetype;
